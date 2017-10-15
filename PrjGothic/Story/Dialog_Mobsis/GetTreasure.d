9c8710cb931c22b08a764e334995163d3c7da786
НастасьСанна <nastassanna@gmail.com>
2017-10-16 01:08:10 +0400

    Эрол-гробокопатель
    Добавлены метки, была ли уже раскопана конкретная могила
    #46

--- PrjGothic/Story/Dialog_Mobsis/GetTreasure.d
+++ PrjGothic/Story/Dialog_Mobsis/GetTreasure.d
@@ -1,5 +1,10 @@
 
+var int GetTreasure_Erol_01_Once;
+var int GetTreasure_Erol_02_Once;
+var int GetTreasure_Erol_03_Once;
+
 var int PC_Erol_GetRing_Run;
+
 instance DIA_PC_Erol_GetRing(C_Info)
 {
 	npc = PC_Erol;
@@ -16,7 +21,7 @@ func void DIA_PC_Erol_GetRing_info()
 {
 	CreateInvItem(self,ItRi_HP_Regen);
 	self.aivar[AIV_INVINCIBLE] = FALSE;
-	PC_Erol_GetRing_Run = -1;
+	PC_Erol_GetRing_Run = FALSE;
 	AI_StopProcessInfos(self);
 };
 //----------------------------
@@ -38,65 +43,67 @@ func void DIA_PC_Erol_DontGetRing_info()
 	//Аданос благословляет добрые дела!
 	B_Adanos_Bless4Good();
 	self.aivar[AIV_INVINCIBLE] = FALSE;
-	PC_Erol_GetRing_Run = -1;
+	PC_Erol_GetRing_Run = FALSE;
 	AI_StopProcessInfos(self);
 };
 
 
 // Эрол роется в могилах ====================================================
 
-func int CanGetTreasure()
+func int CanGetTreasure(var int once)
 {
 	if (!Npc_IsPlayer(self))	{
 		return FALSE;
 	};
 	if (!C_HeroIs_Erol())	{
+		PrintScreen("Здесь уже кто-то побывал...",-1,-1,FONT_ScreenSmall,3);
+		return FALSE;
+	};
+	if (once) {
+		PrintScreen("Я тут уже копал.",-1,-1,FONT_ScreenSmall,3);
 		return FALSE;
 	};
 	if (!Npc_HasItems(self,ItMw_2H_Axe_L_01))	{
-		PrintScreen("Мне нужна кирка.",-1,-1,FONT_ScreenSmall,2);
+		PrintScreen("Мне нужна кирка.",-1,-1,FONT_ScreenSmall,3);
 		return FALSE;
 	};
-	Npc_RemoveInvItem(self,ItMw_2H_Axe_L_01);
 	return TRUE;
 };
+
+func int CanGetTreasure_01()
+{
+	return CanGetTreasure(GetTreasure_Erol_01_Once);
+};
+func int CanGetTreasure_02()
+{
+	return CanGetTreasure(GetTreasure_Erol_02_Once);
+};
+func int CanGetTreasure_03()
+{
+	return CanGetTreasure(GetTreasure_Erol_03_Once);
+};
+
 func void GetTreasure_Erol_01_S1()
 {
-	if (Npc_IsPlayer(self) && C_HeroIs_Erol())
-	{
-		PrintScreen("Ничего нет.",-1,-1,FONT_Screen,2);
-	};
+	PrintScreen("Ничего интересного.",-1,-1,FONT_Screen,2);
+	GetTreasure_Erol_01_Once = TRUE;
 };
 
 func void GetTreasure_Erol_02_S1()
 {
-	if (Npc_IsPlayer(self) && C_HeroIs_Erol())
-	{
-		if (PC_Erol_GetRing_Run == FALSE)	{
-			PrintScreen("На пальце скелета поблескивает кольцо.",-1,-1,FONT_Screen,4);
-			//взять или оставить
-			PC_Erol_GetRing_Run = TRUE;
-			self.aivar[AIV_INVINCIBLE] = TRUE;
-			AI_ProcessInfos(self);
-		}
-		else	{
-			PrintScreen("Здесь больше ничего нет.",-1,-1,FONT_Screen,4);
-		};
-	};
+	PrintScreen("На пальце скелета поблескивает кольцо.",-1,-1,FONT_Screen,4);
+	//взять или оставить
+	PC_Erol_GetRing_Run = TRUE;
+	self.aivar[AIV_INVINCIBLE] = TRUE;
+	AI_ProcessInfos(self);
+	GetTreasure_Erol_02_Once = TRUE;
 };
 
 func void GetTreasure_Erol_03_S1()
 {
-	if (Npc_IsPlayer(self) && C_HeroIs_Erol())
-	{
-		if (!Erol_Graves_GotPlates)	{
-			PrintScreen("Здесь кое-что есть.",-1,-1,FONT_Screen,2);
-			CreateInvItems(self,ItWr_OldStonePlate_Erol,2);
-			CreateInvItems(self,ItWr_Stoneplate_Silver,1);
-			Erol_Graves_GotPlates = TRUE;
-		}
-		else	{
-			PrintScreen("Здесь больше ничего нет.",-1,-1,FONT_Screen,4);
-		};
-	};
+	PrintScreen("Здесь кое-что есть.",-1,-1,FONT_Screen,2);
+	CreateInvItems(self,ItWr_OldStonePlate_Erol,2);
+	CreateInvItems(self,ItWr_Stoneplate_Silver,1);
+	Erol_Graves_GotPlates = TRUE;
+	GetTreasure_Erol_03_Once = TRUE;
 };
