10538e6ceafe010587fcd2e609a1e1d5f0a57f77
НастасьСанна <nastassanna@gmail.com>
2019-05-04 00:46:39 +0400

    Ягода тролля

--- PrjGothic/Items/Eatable/IT_Plants_MR.d
+++ PrjGothic/Items/Eatable/IT_Plants_MR.d
@@ -1,6 +1,7 @@
 
 const int Value_BurnTea = 20;
 const int Value_RedMoss = 300;
+const int Value_TrollBerry = 500;
 
 // =========================================
 instance ItPl_BurnTea(C_ITEM)
@@ -37,4 +38,29 @@ instance ItPl_RedMoss(C_ITEM)
 	description = name;
 	text[5] = NAME_Value;
 	count[5] = value;
+};
+
+// =========================================
+instance ItPl_TrollBerry(C_Item)
+{
+	name = "ягода тролл€";
+	mainflag = ITEM_KAT_FOOD;
+	flags = ITEM_MULTI;
+	value = Value_TrollBerry;
+	visual = "itfo_plants_trollberrys_01.3DS";
+	material = MAT_LEATHER;
+	on_state[0] = Use_TrollBerry;
+	scemeName = "FOOD";
+	description = name;
+	text[1] = NAME_Bonus_Str;	count[1] = 2;
+	text[2] = NAME_Bonus_Dex;	count[2] = 1;
+	text[5] = NAME_Value;
+	count[5] = Value_TrollBerry;
+};
+
+
+func void Use_TrollBerry()
+{
+	B_RaiseAttribute(self,ATR_STRENGTH,2);
+	B_RaiseAttribute(self,ATR_DEXTERITY,1);
 };
