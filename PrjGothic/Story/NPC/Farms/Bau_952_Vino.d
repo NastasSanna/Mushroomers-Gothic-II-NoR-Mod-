d4b64ffa141647f010aee01955a79a4e75df73a2
NastasSanna <nastassanna@gmail.com>
2020-08-30 01:36:47 +0400

    #96 Вино и волки
    Дикие животные не будут нападать на пьяного Вино (такого перегара никто не выдерживает)

--- PrjGothic/Story/NPC/Farms/Bau_952_Vino.d
+++ PrjGothic/Story/NPC/Farms/Bau_952_Vino.d
@@ -24,16 +24,19 @@ instance BAU_952_Vino(Npc_Default)
 };
 func void Rtn_Drunk_952()
 {
+	self.aivar[AIV_MM_Behaviour] = BEHAV_Hidden;
 	TA_Stand_Drinking(8,0,22,0,"NW_FARM1_OUT_14");
 	TA_Stand_Drinking(22,0,8,0,"NW_FARM1_OUT_14");
 };
 func void Rtn_Drunk1_952()
 {
+	self.aivar[AIV_MM_Behaviour] = BEHAV_Hidden;
 	TA_Stand_Drinking(8,0,22,0,"NW_FARM1_UP_04");
 	TA_Stand_Drinking(22,0,8,0,"NW_FARM1_UP_04");
 };
 func void Rtn_Drunk2_952()
 {
+	self.aivar[AIV_MM_Behaviour] = BEHAV_Hidden;
 	TA_Stand_Drinking(8,0,22,0,"NW_XARDAS_MONSTER_UP_05");
 	TA_Stand_Drinking(22,0,8,0,"NW_XARDAS_MONSTER_UP_05");
 };
@@ -44,7 +47,8 @@ func void Rtn_Sleep_952()
 };
 func void Rtn_Start_952()
 {
-		self.aivar[AIV_Temper] = self.aivar[AIV_Temper] & ~TEMPER_NoFightParker;
+	self.aivar[AIV_MM_Behaviour] = self.aivar[AIV_MM_Behaviour] & ~BEHAV_Hidden;
+	self.aivar[AIV_Temper] = self.aivar[AIV_Temper] & ~TEMPER_NoFightParker;
 		TA_Sleep			(22,0,8,0,"NW_FARM1_INSTABLE_BED");
 		TA_Stand_ArmsCrossed(8,0,9,0,"NW_FARM1_FIELD_08");
 		TA_Rake_FP			(9,0,12,0,"NW_FARM1_FIELD_08");
